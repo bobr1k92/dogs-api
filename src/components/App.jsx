e189810d01870c9af85da608b360b8579a6154c0
Denis Mazurov <19bobrik92@gmail.com>
2023-06-09 04:55:17 +0300

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,70 @@
-import { Component } from 'react';
 import { Layout } from './Layout';
 import { BreedSelect } from './BreedSelect';
 import { fetchDogByBreed } from 'api';
 import { Dog } from './Dog';
 import { ErrorMessage } from './ErrorMessage';
 import { DogSkeleton } from './DogSkeleton';
+import { useState } from 'react';
 
 const ERROR_MSG = 'Что то пошло не так, перезагрузите страницу😔';
-export class App extends Component {
-  state = {
-    dog: null,
-    isLoading: false,
-    error: null
-  };
-  fetchDog = async breedId => {
+
+export const App = () => {
+  const [dog, setDog] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchDog = async breedId => {
     try {
-      this.setState({ isLoading: true, error: null });
+      setIsLoading(true);
+      setError(null);
       const fetchedDog = await fetchDogByBreed(breedId);
-      console.log(breedId);
-      this.setState({ dog: fetchedDog });
+      // console.log(breedId);
+      setDog(fetchedDog);
     } catch (error) {
-      this.setState({ error: ERROR_MSG });
+      setError(ERROR_MSG);
     } finally {
-      this.setState({ isLoading: false });
+      setIsLoading(false);
     }
   };
 
-  render() {
-    const { dog, isLoading, error } = this.state;
-    return (
-      <Layout>
-        <BreedSelect onSelect={this.fetchDog} />
-        {isLoading && <DogSkeleton />}
-        {error && <ErrorMessage>{error}</ErrorMessage>}
-        {dog && !isLoading && <Dog dog={dog} />}
-      </Layout>
-    );
-  }
-}
+  return (
+    <Layout>
+      <BreedSelect onSelect={fetchDog} />
+      {isLoading && <DogSkeleton />}
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+      {dog && !isLoading && <Dog dog={dog} />}
+    </Layout>
+  );
+};
+
+// export class App extends Component {
+//   state = {
+//     dog: null,
+//     isLoading: false,
+//     error: null
+//   };
+//   fetchDog = async breedId => {
+//     try {
+//       this.setState({ isLoading: true, error: null });
+//       const fetchedDog = await fetchDogByBreed(breedId);
+//       console.log(breedId);
+//       this.setState({ dog: fetchedDog });
+//     } catch (error) {
+//       this.setState({ error: ERROR_MSG });
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   };
+
+//   render() {
+//     const { dog, isLoading, error } = this.state;
+//     return (
+//       <Layout>
+//         <BreedSelect onSelect={this.fetchDog} />
+//         {isLoading && <DogSkeleton />}
+//         {error && <ErrorMessage>{error}</ErrorMessage>}
+//         {dog && !isLoading && <Dog dog={dog} />}
+//       </Layout>
+//     );
+//   }
+// }
--- src/components/BreedSelect.jsx
+++ src/components/BreedSelect.jsx
@@ -1,44 +1,83 @@
 import { fetchBreeds } from 'api';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import Select from 'react-select';
 import { ErrorMessage } from './ErrorMessage';
 
 const ERROR_MSG = 'Что то пошло не так, перезагрузите страницу😔';
 
-export class BreedSelect extends Component {
-  state = {
-    breeds: [],
-    isLoading: false,
-    error: null
-  };
+export const BreedSelect = ({ onSelect }) => {
+  const [breeds, setBreeds] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async componentDidMount() {
-    try {
-      this.setState({ isLoading: true, error: null });
-      const fetchedBreeds = await fetchBreeds();
-      this.setState({ breeds: fetchedBreeds });
-    } catch (error) {
-      this.setState({ error: ERROR_MSG });
-    } finally {
-      this.setState({ isLoading: false });
+  useEffect(() => {
+    async function getBreeds() {
+      try {
+        setIsLoading(true);
+        setError(null);
+        const fetchedBreeds = await fetchBreeds();
+        // setBreeds(prevState => [...prevState, ...fetchedBreeds];
+        setBreeds(fetchedBreeds);
+      } catch (error) {
+        setError(ERROR_MSG);
+      } finally {
+        setIsLoading(false);
+      }
     }
-  }
-  render() {
-    const { breeds, isLoading, error } = this.state;
-    const options = breeds.map(breed => ({
-      value: breed.id,
-      label: breed.name
-    }));
+    getBreeds();
+  }, []);
 
-    return (
-      <div>
-        <Select
-          options={options}
-          isLoading={isLoading}
-          onChange={option => this.props.onSelect(option.value)}
-        />
-        {error && <ErrorMessage>{error}</ErrorMessage>}
-      </div>
-    );
-  }
-}
+  const options = breeds.map(breed => ({
+    value: breed.id,
+    label: breed.name
+  }));
+
+  return (
+    <div>
+      <Select
+        options={options}
+        isLoading={isLoading}
+        onChange={option => onSelect(option.value)}
+      />
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+    </div>
+  );
+};
+
+// export class BreedSelect extends Component {
+//   state = {
+//     breeds: [],
+//     isLoading: false,
+//     error: null
+//   };
+
+//   async componentDidMount() {
+//     try {
+//       this.setState({ isLoading: true, error: null });
+//       const fetchedBreeds = await fetchBreeds();
+//       this.setState({ breeds: fetchedBreeds });
+//     } catch (error) {
+//       this.setState({ error: ERROR_MSG });
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   }
+//   render() {
+//     const { breeds, isLoading, error } = this.state;
+//     const options = breeds.map(breed => ({
+//       value: breed.id,
+//       label: breed.name
+//     }));
+
+//     return (
+//       <div>
+//         <Select
+//           options={options}
+//           isLoading={isLoading}
+//           onChange={option => this.props.onSelect(option.value)}
+//         />
+//         {error && <ErrorMessage>{error}</ErrorMessage>}
+//       </div>
+//     );
+//   }
+// }
